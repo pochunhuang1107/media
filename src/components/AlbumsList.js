f3bb4f2ac80e2ff3df6d34775238081c02e57535
pochun.huang1107@gmail.com <casper@Po-Chuns-MacBook-Pro.local>
2023-03-27 20:20:06 +0800

    redux toolkit query

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,7 +1,38 @@
-
+import { useAddAlbumMutation, useFetchAlbumsQuery } from "../store";
+import Skeleton from './Skeleton';
+import Button from './Button';
+import AlbumsListItem from "./AlbumsListItem";
 
 export default function AlbumsList({ user }) {
-  return (
-    <div>Album for {user.name}</div>
-  )
+    const { data, error, isFetching } = useFetchAlbumsQuery(user);
+    const [addAlbum, results] = useAddAlbumMutation();
+    
+    const handleAddAlbum = () => {
+        addAlbum(user);
+    }
+
+    let content;
+    if (isFetching) {
+        content = <Skeleton className="h-10 w-full" times={3} />
+    } else if (error) {
+        content = <div>Error loading albums.</div>
+    } else {
+        content = data.map(album => {
+            return <AlbumsListItem key={album.id} album={album} />
+        })
+    }
+
+    return (
+        <div>
+            <div className="flex flex-row justify-between items-center m-2">
+                <h3 className="text-lg font-bold">
+                    Album for {user.name}
+                </h3>
+                <Button onClick={handleAddAlbum} loading={results.isLoading}>+ Add Album</Button>
+            </div>
+            <div>
+                {content}
+            </div>
+        </div>
+    )
 }
---
+++ src/store/apis/photosApi.js
@@ -0,0 +1,72 @@
+import { faker } from "@faker-js/faker";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+//Dev only
+// const pause = (duration) => {
+//     return new Promise((resolve) => {
+//         setTimeout(resolve, duration);
+//     });
+// }
+
+const photosApi = createApi({
+    reducerPath: 'photo',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'http://localhost:3005/',
+        fetchFn: async (...args) => {
+            // Remove for production
+            // await pause(1000);
+            return fetch(...args);
+        }
+    }),
+    endpoints(builder) {
+        return {
+            fetchPhotos: builder.query({
+                providesTags: (result, error, album) => {
+                    const tags = result.map(photo => {
+                        return {type: 'Photo', id: photo.id}
+                    })
+                    tags.push({ type: 'AlbumPhoto', id: album.id});
+                    return tags;
+                },
+                query: (album) => {
+                    return {
+                        url: '/photos',
+                        params: {
+                            albumId: album.id
+                        },
+                        method: 'GET'
+                    }
+                }
+            }),
+            addPhoto: builder.mutation({
+                invalidatesTags: (result, error, album) => {
+                    return [{ type: 'AlbumPhoto', id: album.id }];
+                },
+                query: (album) => {
+                    return {
+                        url: '/photos',
+                        body: {
+                            albumId: album.id,
+                            url: faker.image.abstract(150, 150, true),
+                        },
+                        method: 'POST'
+                    }
+                }
+            }),
+            removePhoto: builder.mutation({
+                invalidatesTags: (result, error, photo) => {
+                    return [{ type: 'Photo', id: photo.id }];
+                },
+                query: (photo) => {
+                    return {
+                        url: `/photos/${photo.id}`,
+                        method: 'DELETE'
+                    }
+                }
+            }),
+        }
+    }
+});
+
+export const { useFetchPhotosQuery, useAddPhotoMutation, useRemovePhotoMutation } = photosApi;
+export { photosApi };
